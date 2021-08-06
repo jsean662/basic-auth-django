841137ffd3fa67ac07d2a434aed6e3d4d3b71477
jay-sawant-pro <jaysawant@financepeer.com>
2021-08-06 23:39:18 +0530

    allowed hosts updated

--- basicAuth/settings.py
+++ basicAuth/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
