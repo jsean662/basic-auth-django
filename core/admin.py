485ab08342a7b4d7a3c7a2e7f40e8f7148fcc46d
jay-sawant-pro <jaysawant@financepeer.com>
2021-08-07 00:23:25 +0530

    Export from admin panel for UserLoginHistory model added

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from core.models import UserLoginHistory
+from import_export.admin import ExportActionMixin
+from import_export import resources
 
 
-admin.site.register(UserLoginHistory)
+class UserLoginHistoryResource(resources.ModelResource):
+
+    class Meta:
+        model = UserLoginHistory
+        fields = ['id', 'dateCreated', 'userId__username', 'ip']
+
+class UserLoginHistoryAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = UserLoginHistoryResource
+    search_fields = ('userId__username',)
+    list_display = ['id', 'dateCreated', 'userId', 'ip']
+
+
+admin.site.register(UserLoginHistory, UserLoginHistoryAdmin)
+        
